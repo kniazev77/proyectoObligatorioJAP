6255fd778a5812969b9e83caf0bdb28647f57540
kniazev77 <juankniazev1996@gmail.com>
2022-08-14 23:51:24 -0300

    try

--- js/products.js
+++ js/products.js
@@ -1,4 +1,9 @@
-// const autos = fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
-//    .then(res => res.json())
-//    .then(data => console.log(data))
+const listadoAutos = fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
+    .then(res => res.json())
+    .then(data => console.log(data))
 
+    const prod = data.products
+    const codigo_a_insertar = ""
+for prod in data.products {codigo_a_insertar += '<div>'}
+
+document.getElementById('spinner-wrapper').innerHTML = codigo_a_insertar
