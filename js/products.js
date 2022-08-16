cf88d69c842e9c28838164be500941e1b9f2be9b
kniazev77 <juankniazev1996@gmail.com>
2022-08-16 19:35:32 -0300

    prueba
    prueba

--- js/products.js
+++ js/products.js
@@ -2,8 +2,24 @@ const listadoAutos = fetch('https://japceibal.github.io/emercado-api/cats_produc
     .then(res => res.json())
     .then(data => console.log(data))
 
-    const prod = data.products
-    const codigo_a_insertar = ""
-for prod in data.products {codigo_a_insertar += '<div>'}
 
-document.getElementById('spinner-wrapper').innerHTML = codigo_a_insertar
+var codigo_a_insertar = ""
+for (let i in listadoAutos.data) {
+    codigo_a_insertar += "products[0] + products[1] + products[2] + products[3] + products[4]"
+    
+    console.log(codigo_a_insertar)
+}
+
+const div_modif = document.getElementsByTagName('lds-ring');
+div_modif.innerHTML += codigo_a_insertar
+
+console.log(div_modif)
+
+/* 
+const listadoAutosSTR= JSON.stringify(listadoAutos);
+console.log(listadoAutosSTR);
+
+
+const car_list = listadoAutosSTR.products
+console.log(car_list)
+*/
