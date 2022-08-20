c085898869eec47ee0d27beeeed1c2e3d3549175
kniazev77 <juankniazev1996@gmail.com>
2022-08-20 19:29:25 -0300

    entrega 1 - parte 1

--- js/products.js
+++ js/products.js
@@ -1,25 +1,36 @@
-const listadoAutos = fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
-    .then(res => res.json())
-    .then(data => console.log(data))
+// CREO FUNCION async PARA TRAER LOS DATOS DEL FETCH Y LE INDICO QUE SE TRATA DE UN JSON PARA QUE PUEDA INTERPRETARLO
 
+async function traerDatos() {
+    let llamada = await fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
+    let llamadaInter = await llamada.json();
+ 
+// LLAMO AL <div> DEL HTML CORRESPONDIENTE PARA MANEJAR LA SECCION DONDE QUIERO INSERTAR EL LISTADO DE PRODUCTOS 
 
-var codigo_a_insertar = ""
-for (let i in listadoAutos.data) {
-    codigo_a_insertar += "products[0] + products[1] + products[2] + products[3] + products[4]"
-    
-    console.log(codigo_a_insertar)
+    let insertarListado = document.getElementById('cat-list-container')
+
+// USO UN for PARA RECORRER LOS ELEMENTOS DEL LISTADO JSON QUE TRAJE, OBTENER LOS ATRIBUTOS QUE QUIERO E INSERTARLOS EN LA SECCION
+for (let elemento of llamadaInter.products) {
+   
+
+// TODA LA ESTRUCTURA QUE VA A ADOPTAR CADA PRODUCTO DE LA PAGINA SE DECLARA LUEGO DEL InnerHTML EN FORMATO HTML ENTRE COMILLAS `` 
+// POR CADA PRODUCTO QUE LEA EL JS VA A INSERTAR UNA NUEVA ESTRUCTURA
+
+    insertarListado.innerHTML += `
+    <div class="list-group-item list-group-item-action cursor-active">
+    <div class="row">
+        <div class="col-3">
+            <img src="${elemento.image}" class="img-thumbnail">
+        </div>
+        <div class="col">
+            <div class="d-flex w-100 justify-content-between">
+                <h4 class="mb-1">${elemento.name} - ${elemento.currency} ${elemento.cost}</h4>
+                <small class="text-muted">${elemento.soldCount} Vendidos</small>
+            </div>
+            <p class="mb-1">${elemento.description}</p>
+        </div>
+    </div>
+</div>
+    `
+    }
 }
-
-const div_modif = document.getElementsByTagName('lds-ring');
-div_modif.innerHTML += codigo_a_insertar
-
-console.log(div_modif)
-
-/* 
-const listadoAutosSTR= JSON.stringify(listadoAutos);
-console.log(listadoAutosSTR);
-
-
-const car_list = listadoAutosSTR.products
-console.log(car_list)
-*/
+traerDatos();
